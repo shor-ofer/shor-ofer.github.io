4873434096cbeacf60d2da5261b37eef34df2fad
ofer <ofer@skylinesoft.com>
2021-05-05 21:37:29 +0300

    test_fork

---
+++ test_fork/settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1144991.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 47058927, 47058948, 47058933, 47058959, 47058929 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/47058935/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/47058955/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/47058944/1/ammo.js', 'preload' : true},
+];
