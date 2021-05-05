47a48b582960047b862a6d23d578fce3f225aaae
ofer <ofer@skylinesoft.com>
2021-05-05 20:27:07 +0300

    upload gametest from playcanvas

---
+++ gametest/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "679324.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': true,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 16281205, 16252454, 16252455, 16252456, 16252457, 16268684, 16268730, 16276310, 16281206 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
